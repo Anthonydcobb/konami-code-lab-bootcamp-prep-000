2e27ca6b65d2a3ea019fcdad1947a87bc2e57587
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-04 23:54:22 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,21 +13,20 @@ const codes = [
 
 function init(codes) {
   let codePress = []
+  let index = 0
   document.body.addEventListener('keydown', (event) => {
   //  if (codePress === codes) {
   //    alert('YOU DID IT')
   //  }
   console.log(event.key)
-
-     if (event.key === codes[i]) {
+     if (event.key === codes[index]) {
        codePress.push(event.key)
        console.log(`right key ${event.key}`)
      } else {
        codePress = [];
        console.log('wrong key')
      }
-   
-
+  
  })
  }
 
