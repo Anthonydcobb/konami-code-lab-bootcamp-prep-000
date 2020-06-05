672e0023173a62df39e810e65148f29a29dffae4
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-05 00:07:54 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -20,7 +20,7 @@ function init(codes) {
        index ++;
        console.log(codePress)
        if (index === codes.length) {
-         alert('YOU DID IT')
+         alert('Hurray!')
        }
      }
      else {
